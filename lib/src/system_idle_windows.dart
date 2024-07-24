f2f2b9be93a627a8b034e1bf8fdaa7f246e4793d
Levi Lesches <levi.lesches@kdab.com>
2024-07-24 16:52:45 -0400

    Some cleanups

--- lib/src/system_idle_windows.dart
+++ lib/src/system_idle_windows.dart
@@ -3,21 +3,23 @@ import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 import 'package:system_idle/src/system_idle.dart';
-import 'package:win32/win32.dart';
+import 'package:win32/win32.dart' as win32;
 
 class SystemIdleCheckerWindows extends SystemIdleChecker {
   final _controller = StreamController<bool>.broadcast();
-
+  final _arena = Arena();
   bool _isIdle = false;
+
   @override
   Future<void> initialize({int time = defaultIdleTime}) async {
-    final plii = calloc<LASTINPUTINFO>()..ref.cbSize = sizeOf<LASTINPUTINFO>();
+    final inputInfo = _arena<win32.LASTINPUTINFO>();
+    inputInfo.ref.cbSize = sizeOf<win32.LASTINPUTINFO>();
 
     Timer.periodic(const Duration(seconds: 1), (timer) async {
       final ticks = getTickCountFunc();
-      GetLastInputInfo(plii);
+      win32.GetLastInputInfo(inputInfo);
 
-      final idleFor = (ticks - plii.ref.dwTime) / 1000;
+      final idleFor = (ticks - inputInfo.ref.dwTime) / 1000;
 
       if (idleFor > time) {
         if (_isIdle) return;
@@ -36,5 +38,4 @@ class SystemIdleCheckerWindows extends SystemIdleChecker {
 }
 
 final DynamicLibrary kernel32 = DynamicLibrary.open("kernel32.dll");
-final getTickCountFunc =
-    kernel32.lookupFunction<Uint32 Function(), int Function()>("GetTickCount");
+final getTickCountFunc = kernel32.lookupFunction<Uint32 Function(), int Function()>("GetTickCount");
